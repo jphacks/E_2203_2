d1204db9347c087be84c6eeec8026e2e0f560156
ippan31 <ippan5392@gmail.com>
2022-10-20 22:05:12 +0900

    aaa

--- class.py
+++ class.py
@@ -1,25 +1,32 @@
 import numbers
 import pandas as pd
+import numpy as np
+from pathlib import Path
+
+
 #データフレーム読み込み
-df = pd.read_csv("jphack/test.csv")
+
+path = 'jphack/test.csv'
+
+df = pd.read_csv(path)
+
+print(df)
 
 print(df)  #最初の5行のデータ表示
 print(df.describe)  #データの概要を数値化
 
-#でかいデータの大小関係を標準化する
+
+#大小関係を標準化する
 
 from sklearn.preprocessing import StandardScaler
 
 from sklearn.preprocessing import StandardScaler
+
 sc = StandardScaler()
-df_sc = sc.fit_transform(df)
-df_sc = pd.DataFrame(df_sc, columns=df.columns)
+df_sc = sc.fit_transform(df.drop('food',axis = 1))
+df_sc = pd.DataFrame(df_sc)
 df.head()
 
-columns = ['id','food','time','temperature','humidity']
-df = df[columns]
-
-df.isnull().sum()
 
 
 #クラスタリング実行
@@ -40,30 +47,140 @@ print(cluster)
 #分布グラフ表示
 
 from sklearn.decomposition import PCA
-pca = PCA(n_components=2, random_state=1)
+from sklearn.cluster import KMeans
+
+pca = PCA(n_components=2, random_state=1) #主成分分析の次元削減(PCA)実行
 pca.fit(df_sc)
 feature = pca.transform(df_sc)
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #グラフ作成
+
 plt.figure(figsize=(6, 6))
 plt.scatter(feature[:, 0], feature[:, 1], alpha=0.8, c=cluster)
 plt.xlabel('principal component 1')
 plt.ylabel('principal component 2')
 plt.show()
 
-#一番下のidから属するクラスを選択
+
+#一番下のデータと同じクラスタのデータを取得
 
 print(df.tail(1))  #tail(1):末尾の行を指定
 
 print(df[-1:]) #これでも末尾の行指定ができる
 
-print(df.at[df.index[-1],'cluster'])
+print(df.at[df.index[-1],'cluster']) #末尾の行のクラスタを出力するやつ
 
-n = df.at[df.index[-1],'cluster']
+n = df.at[df.index[-1],'cluster'] #指定した番号のクラスタ番号取得
 
-df[df['cluster']== n]
+cluster_n =  df[df['cluster']== n]
+
+print(cluster_n)
 
 print(df[df.cluster == n])
+
+
+
+#同じクラスタのアイテムに絞ってXGBoost
+
+# XGBoost
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+
+df_x = df.drop(['cluster'], axis=1)
+df_y = df['cluster']
+
+print(df_x)
+print(df_y)
+
+train_x, test_x, train_y, test_y = train_test_split(df_x, df_y, random_state=1)
+
+dtrain = xgb.DMatrix(train_x, label=train_y)
+dtest  = xgb.DMatrix(test_x, label=test_y)
+
+params = {'objective':'reg:linear',
+          'silent':1,
+          'random_state':1}
+num_round = 50
+
+watchlist = [(dtrain, 'train'), (dtest, 'test')]
+
+#学習
+model = xgb.train(params, dtrain, num_round, verbose_eval=10, evals=watchlist)
+
+
+
+#推論実行、読み込んだデータはラベルがnull、特徴から推論したラベルをぶち込む
+
+new_data = cluster_n[-1:].drop(['cluster'], axis=1)
+
+print(new_data)
+
+pred = model.predict(xgb.DMatrix(new_data))
+
+print(pred)
+
+
+new_label = pred[0]
+
+new_label = round(new_label)
+
+
+print(new_label)
+
+
+#モデルの保存
+
+import os
+import pickle
+
+file_name = "xgb_model.pickle"
+
+pickle.dump(model, open(file_name, "wb"))
+
+
+
+#モデルのサイズ確認
+print(round(os.path.getsize(file_name)/1024/1024,1),'Mb')
+
+
+
+#モデル読み込み 
+loaded_model = pickle.load(open(file_name,'rb'))
+
+pred = loaded_model.predict(xgb.DMatrix(new_data)) #newdataに,送られてきたデータ(dfにして)を挿入
+print(pred)
+
+#配列から数値だけ取り出す
+new_label = pred[0]
+
+new_label = round(new_label)
+
+#最終的に送信するデータ
+recommend_data = df[df['cluster']== new_label]
+
+print(recommend_data)
+
+i = len(recommend_data) #出力したラベルの行数を取得
+
+print(len(recommend_data))
+
+
+recommend_df = recommend_data['food'].value_counts()
+
+print(recommend_df)
+
+
+
+cluster_n =  df[df['cluster']== n]
+
+
+
+from sklearn.metrics import accuracy_score
+
+pred = model.predict(dtest)
+score = accuracy_score(test_y, pred) #精度の確認できん　　なんでだよ
+print(f'{score:.4f}')
+
 
 
 
